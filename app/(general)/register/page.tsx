a8b1172048665ce361a53056d4ce934e144c4bf5
claudierio <claudierio@example.com>
2024-07-15 11:35:11 -0300

    feat: add responsividade para as pags login e cadastro

--- app/(general)/register/page.tsx
+++ app/(general)/register/page.tsx
@@ -2,10 +2,11 @@
 
 import React from "react";
 import styles from "../formStyles.module.scss";
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"; // Adicione esta linha para importar o ícone de seta
 import Link from "next/link";
 
 export default function Register() {
@@ -13,82 +14,94 @@ export default function Register() {
     <div className={styles.container}>
       <div className={styles.leftSide}>
         <div className={styles.loginBox}>
-          <h1>Registre-se</h1>
+          <div className={styles.title}>
+            <h1>Registre-se</h1>
+          </div>
           <form>
             <div className={styles.formGroup}>
               <div className={styles.inputWrapper}>
-                <input 
-                  type="text" 
-                  id="name" 
-                  name="name" 
-                  placeholder="Nome" 
-                  className={styles.input} 
-                  required 
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Nome"
+                  className={styles.input}
+                  required
                 />
                 <PersonOutlineOutlinedIcon className={styles.icon} />
               </div>
             </div>
             <div className={styles.formGroup}>
               <div className={styles.inputWrapper}>
-                <input 
-                  type="date" 
-                  id="birthdate" 
-                  name="birthdate" 
-                  className={styles.input} 
-                  required 
+                <input
+                  type="date"
+                  id="birthdate"
+                  name="birthdate"
+                  className={styles.input}
+                  required
                 />
                 <CalendarMonthOutlinedIcon className={styles.icon} />
               </div>
             </div>
             <div className={styles.formGroup}>
               <div className={styles.inputWrapper}>
-                <input 
-                  type="email" 
-                  id="email" 
-                  name="email" 
-                  placeholder="Email" 
-                  className={styles.input} 
-                  required 
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  className={styles.input}
+                  required
                 />
                 <EmailOutlinedIcon className={styles.icon} />
               </div>
             </div>
             <div className={styles.formGroup}>
               <div className={styles.inputWrapper}>
-                <input 
-                  type="password" 
-                  id="password" 
-                  name="password" 
-                  placeholder="Senha" 
-                  className={styles.input} 
-                  required 
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Senha"
+                  className={styles.input}
+                  required
                 />
                 <LockOutlinedIcon className={styles.icon} />
               </div>
             </div>
             <div className={styles.formGroup}>
               <div className={styles.inputWrapper}>
-                <input 
-                  type="password" 
-                  id="confirmPassword" 
-                  name="confirmPassword" 
-                  placeholder="Confirmar Senha" 
-                  className={styles.input} 
-                  required 
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Confirmar Senha"
+                  className={styles.input}
+                  required
                 />
                 <LockOutlinedIcon className={styles.icon} />
               </div>
             </div>
             <div className={styles.formGroup}>
-              <div className={styles.inputWrapper} >
-                <select id="role" name="role" className={styles.selection} required>
-                  <option value="" disabled selected>Você é:</option>
+              <div className={styles.selectWrapper}>
+                <select
+                  id="role"
+                  name="role"
+                  className={styles.selection}
+                  required
+                >
+                  <option value="" disabled selected>
+                    Você é:
+                  </option>
                   <option value="agiota">Agiota</option>
                   <option value="devedor">Devedor</option>
                 </select>
+                <ArrowDropDownIcon className={styles.icon} />
               </div>
             </div>
-            <button type="submit" className={styles.loginButton}>Proximo</button>
+            <button type="submit" className={styles.loginButton}>
+              Proximo
+            </button>
           </form>
         </div>
       </div>
