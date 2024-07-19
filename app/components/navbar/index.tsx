42baa13a83ed31bdff1969b694570e0d8b72b489
claudierio <claudierio@gmail.com>
2024-07-19 14:35:06 -0300

    feat: adicionado coloração a página visitada no navbar

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import navlogoteste from "@/public/logo_nav.png";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
-import Avatar from "../../../public/avatar_default.png"
+import Avatar from "../../../public/avatar_default.png";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <div className={styles.toplevel}>
       <Link href="/">
@@ -16,17 +19,17 @@ export default function Navbar() {
       </Link>
       <nav className={styles.navbar}>
         <div className={styles.navleft}>
-          <Link className={styles.navLink} href="/">
+          <Link href="/" className={`${styles.navLink} ${pathname === '/' ? styles.active : ''}`}>
             <HomeIcon />
             <span>Início</span>
           </Link>
 
-          <Link className={styles.navLink} href="/about">
+          <Link href="/about" className={`${styles.navLink} ${pathname === '/about' ? styles.active : ''}`}>
             <PersonOutlineOutlinedIcon />
             <span>Quem somos</span>
           </Link>
 
-          <Link className={styles.navLink} href="/emprestimo">
+          <Link href="/emprestimo" className={`${styles.navLink} ${pathname === '/emprestimo' ? styles.active : ''}`}>
             <AttachMoneyOutlinedIcon />
             <span>Empréstimo</span>
           </Link>
